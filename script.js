9f657cdeeba0d2eb0484d13aa46227e526de4342
Jesus Diaz <jesusdiazg9@gmail.com>
2021-05-01 10:03:52 -0500

    comentario, var, let, constante

---
+++ script.js
@@ -0,0 +1,73 @@
+//hola mundo
+//alert('soy javascript');
+
+//comentarios
+
+//comentario de linea
+/*comentario de mas
+de una linea */
+
+
+//sentencias
+
+//alert('hola'); alert('mundo');
+
+//alert('hola');
+//alert('mundo');
+
+//alert('hola')
+//alert('mundo')
+
+//variables
+
+let mensaje;
+mensaje ="hola mundo"
+alert(mensaje)
+
+//let a=5;
+//let b=6;
+//let la suma = a + b;
+//console.log("el resultado de la suma es: ", suma)
+
+let nombre = "Jesus";
+let nombre2= nombre;
+
+nombre2= "carlo";
+console.log("el nombre uno es:",nombre);
+console.log("el nombre dos es:", nombre2);
+
+
+//nombrar variable con camel case
+nombreunno = "jesus";
+nombredos = "manuel";
+
+//nombrar con underscore
+nombre_uno = "jesus";
+nombre_dos = "manuel";
+
+//nombres no comunes
+let $ =1;
+let _ =2;
+
+//no se puede nombrar variable
+/* let 1a =3;
+let primer-nombre =4;*/
+
+"use strict"
+mensaje = "hola mundo";
+
+//constante
+const PI = 3.1416;
+const COLOR_ROJO = "#fffff";
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi);
+console.log(pi_dos);
+
+const id_account = get_id_account(45);
+
+
+
+
+
+
